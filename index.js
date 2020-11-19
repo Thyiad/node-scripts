ab98f3826db6097951623dc334daaf32a115534a
Thyiad <1520583107@qq.com>
2020-11-20 00:07:20 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,17 @@
+const cpAssets = require('./lib/cp-assets');
+const dirClear = require('./lib/dir-clear');
+const dirHandle = require('./lib/dir-handle');
+const dirUglify = require('./lib/dir-uglify');
+const ftp = require('./lib/ftp');
+const tsCompile = require('./lib/ts-compile');
+const commonBuild = require('./lib/common-build');
+
+module.exports = {
+    cpAssets,
+    dirClear,
+    dirHandle,
+    dirUglify,
+    ftp,
+    tsCompile,
+    commonBuild,
+}
